bdadb4111bd009bd769734eed3d16bcf11e26c8c
Leon Koth <leokoth@stagedates.com>
2024-07-02 10:43:23 +0200

    chore: run formatter

--- lib/acx/enforcer_server.ex
+++ lib/acx/enforcer_server.ex
@@ -64,7 +64,7 @@ defmodule Acx.EnforcerServer do
 
   See `Enforcer.load_policies!/2` for more details.
   """
-  def load_policies(ename, pfile) do
+  def load_policies(ename, pfile \\ nil) do
     GenServer.call(via_tuple(ename), {:load_policies, pfile})
   end
 
@@ -84,7 +84,7 @@ defmodule Acx.EnforcerServer do
   See `Enforcer.save_policies/1`
   """
   def save_policies(ename) do
-    GenServer.call(via_tuple(ename),{:save_policies})
+    GenServer.call(via_tuple(ename), {:save_policies})
   end
 
   @doc """
@@ -251,7 +251,7 @@ defmodule Acx.EnforcerServer do
   end
 
   def handle_call({:save_policies}, _from, enforcer) do
-    new_enforcer = enforcer |> Enforcer.save_policies
+    new_enforcer = enforcer |> Enforcer.save_policies()
     {:reply, :ok, new_enforcer}
   end
 
@@ -325,7 +325,8 @@ defmodule Acx.EnforcerServer do
 
   def handle_call({:set_persist_adapter, adapter}, _from, enforcer) do
     case Enforcer.set_persist_adapter(enforcer, adapter) do
-      {:error, reason} -> {:reply, {:error, reason}, enforcer}
+      {:error, reason} ->
+        {:reply, {:error, reason}, enforcer}
 
       new_enforcer ->
         :ets.insert(:enforcers_table, {self_name(), new_enforcer})
