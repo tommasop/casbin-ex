615038f948da2bdbb879d9a1ed8dd1c7162e97b6
Leon Koth <leokoth@stagedates.com>
2024-07-10 14:13:58 +0200

    feat: add release config

--- mix.exs
+++ mix.exs
@@ -3,12 +3,14 @@ defmodule Acx.MixProject do
 
   def project do
     [
-      app: :acx,
-      version: "0.1.1",
-      elixir: "~> 1.13",
+      app: :ex_casbin,
+      version: "0.1.0",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -27,9 +29,24 @@ defmodule Acx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto_sql, "~> 3.11"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.11"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Stagedates Casbin library."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "ex_casbin",
+      organization: "stagedates",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Proprietary"],
+      links: %{"GitHub" => "https://github.com/stagedates/ex_casbin"}
     ]
   end
 end
